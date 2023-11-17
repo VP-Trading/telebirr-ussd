c572703c4d7b8b16cb67d84f4f93dfa1abb8b27d
Alazar Kassahun <alazar.kas@gmail.com>
2023-11-17 16:53:52 +0300

    package for telebirr ussd done

---
+++ config/telebirrussd.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'third_party_id' => env('TELEBIRR_USSD_THIRD_PARTY_ID', ''),
+
+    'password' => env('TELEBIRR_USSD_PASSWORD', ''),
+
+    'result_url' => env('TELEBIRR_USSD_RESULT_URL', ''),
+
+    'operator_id' => env('TELEBIRR_USSD_OPERATOR_ID', ''),
+
+    'security_credentials' => env('TELEBIRR_USSD_SECURITY_CREDENTIALS', ''),
+
+    'short_code' => env('TELEBIRR_USSD_SHORT_CODE', ''),
+
+    'url' => env('TELEBIRR_USSD_URL', '')
+];
---
+++ src/TelebirrUssdServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Vptrading\TelebirrUssd;
+
+use Illuminate\Support\ServiceProvider;
+
+class TelebirrUssdServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/telebirrussd.php' => config_path('telebirrussd.php')
+        ]);
+    }
+
+    public function register()
+    {
+        $this->app->singleton(TelebirrUssd::class, function () {
+            return new TelebirrUssd();
+        });
+    }
+}
